046aeea6145f772ff54a52b4fcf6ae3d2800843f
Jason Thompson <jason@jthompson.ca>
2014-03-14 15:36:26 -0400

    Welcome page displays current date

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+  def machinize_date(date)
+    date.rfc3339
+  end
+
+  def humanize_date(date)
+    date.strftime("%B %-d, %Y")
+  end
 end
--- spec/features/user_opens_home_page_spec.rb
+++ spec/features/user_opens_home_page_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
+require 'date'
 
 feature 'User opens home page' do
   scenario 'they see the current date on the page' do
+    date = Date._strptime(DateTime.now.to_s, "%M %d, %Y")
     visit '/' 
-    expect(page).to have_content('March 14, 2014')
+    expect(page).to have_selector('time')
+    expect(page).to have_content(date)
   end
 end
