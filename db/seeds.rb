781e0357cdfe8dcf34d54fc02599b078e7fa4950
Jason Thompson <jason@jthompson.ca>
2014-03-31 10:14:42 -0400

    Add seed data for events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+events = [{ date_start: "2014-03-15T9:0:0",
+            date_finish: "2014-03-15T9:30:0",
+            summary: "Meeting with Keop" },
+
+          { date_start: "2014-03-15T12:0:0",
+            date_finish: "2014-03-15T12:30:0",
+            summary: "Defeat Zoltar" }]
+
+events.each do |e|
+  Event.create(e)
+end
